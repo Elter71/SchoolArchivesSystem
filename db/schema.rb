0019a7acdc06587cdcf415ccba46ef0abedac936
Piotr Wodziński <pitas333@gmail.com>
2017-10-31 19:39:06 +0100

    Add post/new view with slim gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019192655) do
+ActiveRecord::Schema.define(version: 20171031090759) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
