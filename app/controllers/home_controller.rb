fbc2c90ee9f8c6ecb7564efba9c018b4510fe71d
Piotr Wodziński <wodzinskiipiotr@gmail.com>
2017-11-01 19:45:38 +0100

    Add post list view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+
   def index
+    @post = Post.all
+    @post.each do |id|
+      id.thumbnail
+    end
   end
 end
