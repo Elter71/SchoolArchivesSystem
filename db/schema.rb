c0f83115642d531d4d4edf0973f758a3df2d0679
Piotr Wodziński <wodzinskiipiotr@gmail.com>
2017-11-11 10:32:06 +0100

    Add active column to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107182206) do
+ActiveRecord::Schema.define(version: 20171111092831) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20171107182206) do
     t.string "first_name"
     t.string "last_name"
     t.integer "role_id"
+    t.boolean "active", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
