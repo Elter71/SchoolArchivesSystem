aa28545a62eebb0c87fe0e333b69f77695247708
Piotr Wodziński <wodzinskiipiotr@gmail.com>
2017-11-11 06:27:39 +0100

    Add post fist file react components

--- app/assets/javascripts/components/files_list.js.jsx
+++ app/assets/javascripts/components/files_list.js.jsx
@@ -1,9 +1,60 @@
 class FilesList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: this.props.id_post,
+            files: []
+        };
+        this.get_files(this.state.id)
+    }
 
+    get_files(id) {
+        axios({
+            method: 'get',
+            url: '/files/' + id + '',
+            responseType: 'json'
+        })
+            .then(response => {
+                this.set_files_into_state(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    set_files_into_state(data) {
+
+        this.setState({
+            files: data
+        });
+    }
+
+    download_zip() {
+        var path = location.protocol + '//' + location.host + '/files/' + this.state.id + "/download";
+        console.log("PATH" + path);
+        window.location = path;
+    }
 
     render() {
+        var rows = [];
+        for (var i = 0; i < this.state.files.length; i++) {
+            rows.push(
+                <a href={"/file/" + this.state.id + "/" + this.state.files[i]}
+                   className="list-group-item">{this.state.files[i]}</a>
+            )
+        }
+        if (rows.length > 0) {
+            rows.push(
+                <div className="list-group-item">
+                    <button onClick={() => this.download_zip()} className="btn btn-primary">
+                        Pobierz wszytkie pliki
+                    </button>
+                </div>);
+        }
         return (
-            <div> Files</div>
+            <div className="list-group">
+                {rows}
+            </div>
         )
     }
 }
