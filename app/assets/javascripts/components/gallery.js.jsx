e269f89c71d69f12dd3a0f8dc92f7d90f96744f2
Piotr Wodziński <wodzinskiipiotr@gmail.com>
2017-11-11 06:28:05 +0100

    Add post gallery react component

--- app/assets/javascripts/components/gallery.js.jsx
+++ app/assets/javascripts/components/gallery.js.jsx
@@ -1,9 +1,47 @@
 class Gallery extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: this.props.id_post,
+            images: []
+        };
+        this.get_images(this.state.id);
+    }
 
+    get_images(id) {
+        axios({
+            method: 'get',
+            url: '/files/' + id + '/gallery',
+            responseType: 'json'
+        })
+            .then(response => {
+                this.set_images_into_state(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    set_images_into_state(data) {
+
+        this.setState({
+            images: data
+        });
+    }
 
     render() {
+        var rows = [];
+        for (var i = 0; i < this.state.images.length; i++) {
+            rows.push(
+                <div className="col-sm-3 center-block" style={{height: 200 + 'px', marginTop: 10 + 'px'}}>
+                    <img className="img-thumbnail center"
+                         style={{maxHeight: 200 + 'px', maxWidth: 200 + 'px'}}
+                         src={"http://0.0.0.0:3000/file/" + this.state.id + "/" + this.state.images[i]} max/>
+                </div>
+            )
+        }
         return (
-            <div> Gallery</div>
+            <div>{rows}</div>
         )
     }
 }
