14f6ba4ba87a7a912de91ab9897fbe715dd07f3a
Piotr Wodziński <wodzinskiipiotr@gmail.com>
2017-11-12 18:53:04 +0100

    Add delete method to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   validates :title, :description, presence: true
   belongs_to :user
 
+  def destroy
+    super
+    FileUtils.rm_rf(Configuration.instance.server_path+"#{id}/")
+  end
+
+  def delete
+    super
+    destroy
+  end
+
 end
