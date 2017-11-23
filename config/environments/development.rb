1cf0064b82efe12ce0f6b7cf48093e06c56842bc
Piotr Wodziński <wodzinskiipiotr@gmail.com>
2017-11-23 09:16:02 +0100

    Add forgot password configuration and page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,14 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  ActionMailer::Base.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => "",
+      :password             => "",
+      :authentication       => "plain"
+  }
   config.i18n.default_locale = :pl
 end
